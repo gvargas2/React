d378545857fe336accecbd54a84fb6aab76f9dea
Gabriela Vargas <44576817+gvargas2@users.noreply.github.com>
2019-11-26 17:07:10 -0300

    42° commit: Function components 2, cambiamos la funcion de square, para hacerla más sencilla 6, error comparando con codigo

--- tictactoe/index.js
+++ tictactoe/index.js
@@ -1,18 +1,6 @@
 'use strict'
 
-  //Agrego un "constructor a la clase para inicializar el "estado"*/
-  /*Se eliminó el constructor porque Square ya no llega en segimiento del estado del juego
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
-  */
-
-  //render() {
   function Square(props) {
     return (
       //Al clikear cualquier botón, me salta una alerta
@@ -30,16 +18,6 @@
 //{this.state.value}
 //Cambiamos this.state, por this.props --> Tuvimos que bajar esta anotación.
 
-/* onClick={() => this.setState({value: "X"})}>
-onClick={() => this.props.onClick()}
->
-{this.props.value}
-
-Cambiamos los this.props por props
-
-Se elimino para hacer una funcion Square
-class Square extends React.Component
-*/
 
 class Board extends React.Component {
 
